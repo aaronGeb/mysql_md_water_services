04aff61e7d4e1675096bf18a21118533d8de57c7
Aaron Gebremaraim <mulabr673@gmail.com>
2023-10-20 17:01:45 +0300

    Integrated projets

---
+++ query_insight.sql
@@ -0,0 +1,127 @@
+SET SQL_SAFE_UPDATES = 0;
+
+USE md_water_services;
+/*
+1. Get to know our data:
+Start by retrieving the first few records from each table.
+ * How many tables are there in our database? 
+ * What are the names of these tables? Once you've identified the tables, 
+ write a SELECT statement to retrieve the first five records from each table. 
+ As you look at the data, take note of the columns and their respective data types in each table. 
+ What information does each table contain?
+*/
+SHOW TABLES;
+SELECT * FROM  employee LIMIT 5;
+SELECT * FROM  global_water_access LIMIT 5;
+SELECT * FROM  location  LIMIT 5;
+SELECT * FROM  visits LIMIT 5;
+SELECT * FROM  water_quality LIMIT 5;
+SELECT * FROM  water_source LIMIT 5; 
+SELECT * FROM  well_pollution LIMIT 5;
+-- SELECT * FROM  data_dictionary;
+
+/*
+2. Dive into the water sources:
+ Now that you're familiar with the structure of the tables, let's dive deeper.
+ We need to understand the types of water sources we're dealing with.
+ Can you figure out which table contains this information?
+*/
+SELECT DISTINCT type_of_water_source  FROM  water_source ;
+/*
+3. Unpack the visits to water sources:
+   We have a table in our database that logs the visits made to different water sources.
+   Can you identify this table?
+*/
+SELECT * FROM visits WHERE time_in_queue >500;
+SELECT source_id, type_of_water_source,number_of_people_served
+FROM water_source
+GROUP BY source_id,number_of_people_served,type_of_water_source;    
+/*
+4. Assess the quality of water sources:
+The quality of our water sources is the whole point of this survey.
+We have a table that contains a quality score for each visit made about a water source that was assigned by a Field surveyor.
+They assigned a score to each source from 1, being terrible, to 10 for a good, clean water source in a home.
+Shared taps are not rated as high, and the score also depends on how long the queue times are.
+Look through the table record to find the table.
+*/
+SELECT  subjective_quality_score,visit_count FROM  water_quality
+WHERE subjective_quality_score =10 AND visit_count =1
+
+/*
+5. Investigate pollution issues:
+Did you notice that we recorded contamination/pollution data for all of the well sources?
+ Find the right table and print the first few rows.
+*/;
+ 
+ 
+SELECT * FROM well_pollution
+LIMIT 5;
+SELECT DISTINCT description FROM well_pollution
+ORDER BY description;
+/*
+Update descriptions that mistakenly mention
+`Clean Bacteria: E. coli` to `Bacteria: E. coli`
+*/
+UPDATE  well_pollution
+SET description = "Bacteria: E. coli"
+WHERE description = "Clean Bacteria: E. coli";
+/*
+Update the descriptions that mistakenly mention
+`Clean Bacteria: Giardia Lamblia` to `Bacteria: Giardia Lamblia
+*/
+UPDATE  well_pollution
+SET description = "Bacteria: Giardia Lamblia"
+WHERE description = "Clean Bacteria: Giardia Lamblia";
+/*
+Update the `result` to `Contaminated: Biological` where
+`biological`  is greater than 0.01 plus current results is `Clean`
+*/
+UPDATE  well_pollution
+SET results = "Contaminated: Biological"
+WHERE  biological >0.01 AND results = "clean";
+-- Check if our errors are fixed using a SELECT query on the well_pollution_copy table:
+SELECT
+*
+FROM
+    well_pollution
+WHERE
+description LIKE "Clean_%"
+OR (results = "Clean" AND biological > 0.01);
+
+-- Q1.
+SELECT address FROM employee 
+WHERE employee_name =  "Bello Azibo";
+-- Q2.
+SELECT * FROM employee 
+WHERE position = "Micro biologist";
+-- Q3.
+SELECT source_id,number_of_people_served FROM water_source
+ORDER BY number_of_people_served DESC
+limit 3;
+-- Q4.
+SELECT * FROM global_water_access
+WHERE name = "Maji Ndogo";
+-- Q5.
+SELECT *
+FROM employee
+WHERE position = 'Civil Engineer' AND (province_name = 'Dahabu' OR
+address LIKE '%Avenue%');
+			  
+-- Q6.
+SELECT *
+FROM employee
+Where position = "Field Surveyor"
+        AND (phone_number LIKE '%86%' OR "%11%")
+	    AND (employee_name LIKE "%A%" OR "%M%");
+
+-- Q7
+SELECT *
+FROM well_pollution
+WHERE description LIKE 'Clean %' OR results = 'Clean' AND biological < 0.01;
+
+-- Q10.
+SELECT *
+FROM well_pollution
+WHERE description
+IN ('Parasite: Cryptosporidium', 'biologically contaminated')
+OR (results = 'Clean' AND biological > 0.01);
